6e1d158a43c1ea61a71b9a338b2c7c8c3d6b5f0f
Yuto Urushima <yutourushima@YutonoMBP.elecom>
2022-07-02 17:15:00 +0900

    make swagger-cli command

--- bin/command.js
+++ bin/command.js
@@ -1,2 +1,31 @@
 #!/usr/bin/env node
-console.log("これはテストです。")
+const program = require("commander");
+const { execSync } = require('child_process')
+
+program
+.option("-o, --output <s>", "output file pass")
+.option("-t, --type <s>", "file type")
+.option("-i, --input <s>", "input file pass")
+.parse(process.argv);
+
+// make swagger-cli command
+if (program._optionValues['output']
+    && program._optionValues['type']
+    && program._optionValues['input']
+  ) {
+    const cmd = execSync(
+      'npm run swagger-cli '
+      + 'bundle '
+      + '-o '
+      + program._optionValues['output']
+      + ' '
+      + '-t '
+      + program._optionValues['type']
+      + ' '
+      + program._optionValues['input']
+    )
+    console.log(`${cmd.toString()}`)
+
+} else {
+  console.log('options is not enough! check "-h" or "--help"')
+}
